d46ad8048652b84de9ef1034a64d6e9907f27d69
Seba Illingworth <seba.illingworth@gmail.com>
2009-10-15 17:28:32 +1300

    Update db to calculate itinerary sales tax amount

--- TourWriter/Services/Update/DatabaseUpdateScripts/2009.10.14.sql
+++ TourWriter/Services/Update/DatabaseUpdateScripts/2009.10.14.sql
@@ -18,33 +18,184 @@ SET ANSI_NULLS ON
 GO
 ----------------------------------------------------------------------------------------
 GO
-PRINT N'Altering dbo.ItinerarySaleAllocationDetail...';
+PRINT N'Altering dbo.ItineraryPricing...';
 GO
-ALTER VIEW [dbo].[ItinerarySaleAllocationDetail]
+ALTER FUNCTION [dbo].[ItineraryPricing]
+( )
+RETURNS TABLE 
+AS
+RETURN 
+    (	
+	select 
+		ItineraryID,
+		ItemCount,
+		Net,
+		Gross,
+		case when Net = 0 then 0 else (Gross - Net)/Net*100 end as Markup,
+		case when Gross = 0 then 0 else (Gross - Net)/Gross*100 end as Commission,
+		Gross - Net as Yield,
+		GrossOrig,
+		case when Net = 0 then 0 else (GrossOrig - Net)/Net*100 end as MarkupOrig,
+		case when GrossOrig = 0 then 0 else (GrossOrig - Net)/GrossOrig*100 end as CommissionOrig
+	from
+	(
+		select 
+			i.ItineraryID, 
+			isnull(ItemCount,0) as ItemCount, 
+			Net, 
+			case
+				when (GrossOverride is not null) then GrossOverride
+				when (GrossMarkup is not null) then isnull(Gross,0) * (1+GrossMarkup/100)
+				else Gross
+			end as Gross,
+			case
+				when (GrossOverride is not null) then GrossOverride
+				when (GrossMarkup is not null) then isnull(GrossOrig,0) * (1+GrossMarkup/100)
+				else GrossOrig
+			end as GrossOrig
+		from Itinerary i
+		left outer join
+		(
+			select ItineraryID, count(*) as ItemCount, sum(TotalNet) as Net, sum(TotalGross) as Gross, sum(TotalGrossOrig) as GrossOrig
+			from PurchaseItemPricing()
+			group by ItineraryID
+		) p on i.ItineraryID = p.ItineraryID
+	) t
+)
+
+GO
+PRINT N'Creating dbo.ItinerarySaleAllocationPricing...';
+GO
+CREATE FUNCTION [dbo].[ItinerarySaleAllocationPricing]
+( )
+RETURNS TABLE 
+AS
+RETURN 
+    (	
+	select
+		itin.ItineraryId,
+		sale.ItinerarySaleID,
+		stype.ServiceTypeID,
+		alloc.Amount as SaleAmount,
+		case when itin.Markup = -100 then 0 else
+			cast(alloc.Amount*100/(100+itin.Markup) as money) end as Net,
+		case when itin.Markup = -100 then 0 else
+			cast(alloc.Amount - (alloc.Amount*100/(100+itin.Markup)) as money) end as Yield,		
+		case when grossTax.Amount = -100 then 0 else
+			cast(alloc.Amount - (alloc.Amount*100/(100+grossTax.Amount)) as money) end as TaxAmount
+	from ItinerarySaleAllocation alloc
+	right outer join ItinerarySale sale on alloc.ItinerarySaleID = sale.ItinerarySaleID	
+	left outer join dbo.ItineraryPricing() itin on sale.ItineraryID = itin.ItineraryID
+	left outer join ServiceType stype on alloc.ServiceTypeID = stype.ServiceTypeID
+	left outer join TaxType as grossTax on stype.GrossTaxTypeID = grossTax.TaxTypeID
+)
+
+GO
+PRINT N'Altering dbo.ItineraryDetail...';
+GO
+ALTER VIEW [dbo].[ItineraryDetail]
 AS
 select
-	itin.*,
-	sale.ItinerarySaleID,
-	sale.Comments,
-	sale.SaleDate,
-	stype.ServiceTypeName,
-	alloc.Amount as SaleAmount,
-	case when itin.ItineraryMarkup = -100 then 0 else
-		cast(alloc.Amount*100/(100+itin.ItineraryMarkup) as money) end as SaleNet,
-	case when itin.ItineraryMarkup = -100 then 0 else
-		cast(alloc.Amount - (alloc.Amount*100/(100+itin.ItineraryMarkup)) as money) end as SaleYield,		
-	case when grossTax.Amount = -100 then 0 else
-	    cast(alloc.Amount - (alloc.Amount*100/(100+grossTax.Amount)) as money) end as SaleTaxAmount,	
-	grossTax.TaxTypeCode as SaleTaxTypeCode,
-	grossAct.AccountingCategoryCode as AccountingCategoryCode,
-	sale.IsLockedAccounting
-from ItinerarySaleAllocation alloc
-right outer join ItinerarySale sale on alloc.ItinerarySaleID = sale.ItinerarySaleID
-left outer join dbo.ItineraryDetail itin on sale.ItineraryID = itin.ItineraryID
-left outer join ServiceType stype on alloc.ServiceTypeID = stype.ServiceTypeID
-left outer join AccountingCategory as grossAct on stype.GrossAccountingCategoryID = grossAct.AccountingCategoryID
-left outer join TaxType as grossTax on stype.GrossTaxTypeID = grossTax.TaxTypeID;
+	itin.ItineraryID,
+	itin.ItineraryName,
+	itin.DisplayName,
+	itin.CustomCode,
+	itin.CountryID as OriginID,
+	origin.CountryName as Origin,
+	datediff(dd, itin.ArriveDate, itin.DepartDate) + 1 as ItineraryLength,
+	itin.ArriveDate as ArriveDate, 
+	case itin.ArriveDate when null then null else datename(weekday, itin.ArriveDate) end as ArriveDay,
+	case itin.ArriveDate when null then null else datename(month,   itin.ArriveDate) end as ArriveMonth,
+	case itin.ArriveDate when null then null else datename(year,    itin.ArriveDate) end as ArriveYear, 
+	--case itin.ArriveDate when null then null else substring(convert(varchar(10), itin.ArriveDate, 120), 6, 2) end as ArriveMonth,
+	arriveCity.CityName as ArriveCityName,
+	ArriveFlight,
+	itin.DepartDate,
+	departCity.CityName as DepartCityName,
+	DepartFlight,
+	itin.AssignedTo as UserID,
+	u.DisplayName as UserDisplayName,
+	agent.AgentID,
+	agent.AgentName,
+	parentAgent.AgentName as ParentAgentName,
+	agent.Phone as AgentPhone,
+	agent.Email as AgentEmail,
+	agent.VoucherFooter as AgentVoucherNote,
+	stat.ItineraryStatusID,
+	stat.ItineraryStatusName,
+	ItinerarySourceName,
+	usr.UserName as AssignedTo,
+	depart.DepartmentID,
+	depart.DepartmentName,
+	branch.BranchID,
+	branch.BranchName,
+	itin.NetComOrMup as NetOverrideComOrMup,
+	pric.Net as ItineraryNet,
+	pric.Gross as ItineraryGross,
+	pric.Markup as ItineraryMarkup,
+	pric.Commission as ItineraryCommission,
+	pric.Yield as ItineraryYield,
+	pric.GrossOrig as ItineraryGrossOrig,
+	pric.MarkupOrig as ItineraryMarkupOrig,
+	pric.CommissionOrig as ItineraryCommissionOrig,
+	payments.TotalPayments as ItineraryTotalPayments,
+	pric.Gross - payments.TotalPayments as ItineraryTotalOutstanding,
+	sales.TotalSales as ItineraryTotalSales,
+	sales.TotalSalesTax as ItineraryTotalSalesTax,
+	pric.ItemCount as ItineraryPurchaseItemCount,
+	isnull(PaxOverride, pax.PaxCount) as PaxCount,
+	convert(varchar(3), dbo.PaymentTerm.DepositDuePeriod) + ' ' + depdue.PaymentDueName as ItineraryDepositTerms,
+	dbo.GetPaymentDate(itin.ArriveDate, dbo.PaymentTerm.DepositDuePeriod, depdue.PaymentDueName) as ItineraryDepositDueDate, 
+	dbo.PaymentTerm.DepositType as ItineraryDepositType, 
+	dbo.PaymentTerm.DepositAmount as ItineraryDepositAmount,
+	convert(varchar(3), dbo.PaymentTerm.PaymentDuePeriod) + ' ' + baldue.PaymentDueName as ItineraryBalanceTerms,
+	dbo.GetPaymentDate(itin.ArriveDate, dbo.PaymentTerm.PaymentDuePeriod, baldue.PaymentDueName) as ItineraryBalanceDueDate,
+	pric.Gross - dbo.PaymentTerm.DepositAmount as ItineraryBalanceAmount,
+	itin.AddedOn as ItineraryCreatedDate,
+	ParentFolderID as MenuFolderID	
+from Itinerary itin
+join dbo.ItineraryPricing() pric on itin.ItineraryID = pric.ItineraryID
+left outer join Country origin on itin.CountryID = origin.CountryID
+left outer join City arriveCity on itin.ArriveCityID = arriveCity.CityID
+left outer join City departCity on itin.DepartCityID = departCity.CityID
+left outer join Agent agent on itin.AgentID = agent.AgentID
+left outer join Agent parentAgent on agent.ParentAgentID = parentAgent.AgentID
+left outer join dbo.PaymentDue as baldue 
+right outer join dbo.PaymentTerm on baldue.PaymentDueID = dbo.PaymentTerm.PaymentDueID 
+left outer join dbo.PaymentDue as depdue on dbo.PaymentTerm.DepositDueID = depdue.PaymentDueID on isnull(itin.PaymentTermID,agent.SalePaymentTermID) = dbo.PaymentTerm.PaymentTermID
+left outer join ItineraryStatus stat on itin.ItineraryStatusID = stat.ItineraryStatusID
+left outer join ItinerarySource source on itin.ItinerarySourceID = source.ItinerarySourceID
+left outer join [User] usr on itin.AssignedTo = usr.UserID
+left outer join Department depart on itin.DepartmentID = depart.DepartmentID
+left outer join Branch branch on itin.BranchID = branch.BranchID
+left outer join [User] u on u.UserID = itin.AssignedTo
+left outer join
+(
+	select itin.ItineraryID, count(ItineraryMemberID) as PaxCount
+	from ItineraryMember mem
+	left outer join ItineraryGroup grp ON mem.ItineraryGroupID = grp.ItineraryGroupID
+	left outer join Itinerary itin ON grp.ItineraryID = itin.ItineraryID
+	group by itin.ItineraryID
+) pax on itin.ItineraryID = pax.ItineraryID
+left outer join 
+(
+	select ItineraryID, sum(SaleAmount) as TotalSales, sum(TaxAmount) as TotalSalesTax
+	from ItinerarySaleAllocationPricing() sale
+	group by ItineraryID
+) sales on itin.ItineraryID = sales.ItineraryID
+left outer join 
+(
+	select ItineraryID, sum(Amount) as TotalPayments
+	from ItineraryPayment payment
+	left outer join ItineraryMember mem on payment.ItineraryMemberID = mem.ItineraryMemberID
+	left outer join ItineraryGroup grp on mem.ItineraryGroupID = grp.ItineraryGroupID
+	group by ItineraryID
+) payments on itin.ItineraryID = payments.ItineraryID;
 
+GO
+PRINT N'Refreshing dbo.ItineraryPaymentDetail...';
+GO
+EXECUTE sp_refreshview N'dbo.ItineraryPaymentDetail';
 GO
 PRINT N'Altering dbo.ItinerarySaleDetail...';
 
@@ -76,6 +227,10 @@ left outer join
 	group by alloc.ItinerarySaleID
 ) cost on cost.ItinerarySaleID = sale.ItinerarySaleID;
 
+GO
+PRINT N'Refreshing dbo.ItineraryClientDetail...';
+GO
+EXECUTE sp_refreshview N'dbo.ItineraryClientDetail';
 GO
 PRINT N'Altering dbo.PurchaseItemDetail...';
 
@@ -155,6 +310,37 @@ from
 	right outer join PaymentTerm on baldue.PaymentDueID = PaymentTerm.PaymentDueID
 	left outer join PaymentDue as depdue on PaymentTerm.DepositDueID = depdue.PaymentDueID on item.PaymentTermID = PaymentTerm.PaymentTermID
 ) t;
+
+GO
+PRINT N'Refreshing dbo.ItineraryServiceTypeDetail...';
+GO
+EXECUTE sp_refreshview N'dbo.ItineraryServiceTypeDetail';
+GO
+PRINT N'Altering dbo.ItinerarySaleAllocationDetail...';
+GO
+
+ALTER VIEW [dbo].[ItinerarySaleAllocationDetail]
+AS
+select
+	itin.*,
+	sale.ItinerarySaleID,
+	sale.Comments,
+	sale.SaleDate,
+	stype.ServiceTypeName,	
+	alloc.SaleAmount,
+	alloc.Net as SaleNet,
+	alloc.Yield as SaleYield,		
+	alloc.TaxAmount as SaleTaxAmount,	    	
+	grossTax.TaxTypeCode as SaleTaxTypeCode,
+	grossAct.AccountingCategoryCode as AccountingCategoryCode,
+	sale.IsLockedAccounting
+from ItinerarySaleAllocationPricing() alloc
+right outer join ItinerarySale sale on alloc.ItinerarySaleID = sale.ItinerarySaleID
+left outer join dbo.ItineraryDetail itin on sale.ItineraryID = itin.ItineraryID
+left outer join ServiceType stype on alloc.ServiceTypeID = stype.ServiceTypeID
+left outer join AccountingCategory as grossAct on stype.GrossAccountingCategoryID = grossAct.AccountingCategoryID
+left outer join TaxType as grossTax on stype.GrossTaxTypeID = grossTax.TaxTypeID;
+
 GO
 PRINT N'Refreshing dbo.PurchaseItemPaymentsDetail...';
 GO
