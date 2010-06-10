546dfa911cd7cfe2db4ad90535227648a5add62c
Seba Illingworth <seba.illingworth@gmail.com>
2010-06-10 22:02:23 +1200

    Add Gst update database step

--- TourWriter/Modules/Plugins/NzGst/GstAdjust.cs
+++ TourWriter/Modules/Plugins/NzGst/GstAdjust.cs
@@ -1,22 +1,63 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Text;
 using System.Windows.Forms;
 using Infragistics.Win;
 using Infragistics.Win.UltraWinGrid;
 using TourWriter.Info;
 using TourWriter.Info.Services;
-using Resources = Infragistics.Win.UltraWinGrid.Resources;
 
 namespace TourWriter.Modules.Plugins.NzGst
 {
     public partial class GstAdjust : ModuleBase
     {
+        private const string InfoMsg =
+@"Use the NZ GST Adjust screen to adjust prices for the NZ GST change to 15%.
+(to print this text, right-click > select all, right-click > copy, and paste into email/word for printing).
+
+Important considerations:
+1. Supplier rates that span 1 Oct will not be split, they will just be updated to 15% (if you choose to).
+2. The above will probably not matter as pre 1 Oct quotes are most likely almost finished with?
+3. Itinerary bookings that get updated will affect their Itinerary price (unless Itinerary has a final $ override).
+4. For the above, you might choose to only update bookings where Itinerary has status 'Final' (or similar).
+
+Getting started:
+1. Use the combo box to choose which data to load.
+2. Preview the data carefully, you may choose to do manual work (split?) on data that spans 1 Oct.
+3. Consider exporting the list to Excel for future reference (save the trees).
+4. Select which rows to update (click col header to un/select all).
+5. You can update in batches, you don't have to update all at once.
+6. Press the Update button to start the 3 step update process on selected rows.
+7. Safety measures ensure that each row can only be updated once (on screen an in database).
+
+Previewing the data:
+1. System automatically loads only Countries that contain 'zealand' (case insensitive, we only want New Zealand suppliers).
+2. System automatically loads only Gross values that are a valid and positive number.
+3. Supplier data shows rate info as entered (net and markup, net and gross, or gross and commission).
+4. Columns Net15 and Gross15 show the intended price adjustment.
+5. The calculation for these is: ((Amount*100) / (100+12.5)) + (((Amount*100) / (100+12.5)) * 15/100)
+    eg remove old GST 12.5% then add new GST 15%.
+6. Data will be updated as shown, one update per row. Dates that span 1 Oct WILL NOT be split for you.
+
+Running the update:
+1. When you choose to update, only the rows that you select will be updated.
+2. The update process is in 3 steps, you can pull out at any time before the end (ask boss to press the final one ;)
+3. A database backup safety-snapshot is including in one of the update steps.
+3. If the final update step fails, all changes will be rolled back for safety (all or nothing).
+4. After a row is updated, it is protected (disabled) so that you cannot update it a second time.
+
+After the update:
+Processed rows will be disabled when previewed again, so they cannot be updated twice. They are also marked as processed in the database for further safety and future reference. So it is safe to continue to play with these screens and run further updates on any unprocessed rows as required.
+";
         #region sql
         // ---------------------------------------------------------------------------------------------------------------
         const string SupplierSql = @"
 select
+    isnull(GstUpdated, cast(0 as bit)) as GstUpdated,
     s.ParentFolderId as SupplierFolderID,
 	s.SupplierID, 
+    o.OptionID,
 	SupplierName,
 	c.CountryName as Country,
 	v.ServiceName, 
@@ -27,13 +68,13 @@ select
 	case when (PricingOption = 'nm' and Net is not null and Net <> 0) then (Gross - Net)/Net*100 else null end as Markup,	
 	case when (PricingOption = 'gc' and Gross is not null and Gross <> 0) then (Gross - Net)/Gross*100 else null end as Commission,		
 	case when PricingOption = 'nm' or PricingOption = 'ng'	then Net else null end as Net,	
-	case when PricingOption = 'nm' or PricingOption = 'ng'	
+	case when ((GstUpdated is null or GstUpdated = 'false') and (PricingOption = 'nm' or PricingOption = 'ng'))	
 		then cast( ((Net * 100) / (100 + 12.5)) + 	(((Net * 100) / (100 + 12.5)) * 15/100) as money)
 		else null end as Net15,	
 	case when PricingOption = 'ng' or PricingOption = 'gc'	then Gross else null end as Gross,
-	case when PricingOption = 'ng' or PricingOption = 'gc'	
+	case when ((GstUpdated is null or GstUpdated = 'false') and (PricingOption = 'ng' or PricingOption = 'gc'))
 		then cast( ((Gross * 100) / (100 + 12.5)) + (((Gross * 100) / (100 + 12.5)) * 15/100) as money)
-		else null end as Gross15	
+		else null end as Gross15
 	--,GST_old      = ((Gross * 100) / (100 + 12.5)) * 12.5/100
 	--,Gross_exc	=  (Gross * 100) / (100 + 12.5)
 	--,GST_new	    = ((Gross * 100) / (100 + 12.5)) * 15/100
@@ -47,16 +88,18 @@ inner join [Country] c on s.CountryID = c.CountryID and c.CountryName like '%zea
 where {0}
 --where ValidFrom <= '2010.10.1 00:00:00' and ValidTo >= '2010.10.1 00:00:00'
 --where ValidFrom >= '2010.10.1 00:00:00'
-and o.GstUpdated is null or o.GstUpdated = 'false'
+--and o.GstUpdated is null or o.GstUpdated = 'false'
 and Gross is not null and Gross > 0
 order by SupplierName, ServiceName, ValidFrom, OptionName";
 
         // ---------------------------------------------------------------------------------------------------------------
         private const string ItinerarySql = @"
 select 
+    isnull(GstUpdated, cast(0 as bit)) as GstUpdated,
 	i.ParentFolderID as ItineraryFolderID,
 	s.ParentFolderID as SupplierFolderID,
 	i.ItineraryID,
+    p.PurchaseItemID,
 	i.ItineraryName,
 	i.GrossOverride,
 	t.ItineraryStatusName,
@@ -67,9 +110,11 @@ select
 	c.CountryName,
 	r.RequestStatusName,
 	p.Net,
-	cast( ((Net * 100) / (100 + 12.5)) + (((Net * 100) / (100 + 12.5)) * 15/100) as money) as Net15,	
-	p.Gross,	
-	cast( ((Gross * 100) / (100 + 12.5)) + (((Gross * 100) / (100 + 12.5)) * 15/100) as money) as Gross15		
+	case when GstUpdated is null or GstUpdated = 'false' then 
+        cast( ((Net * 100) / (100 + 12.5)) + (((Net * 100) / (100 + 12.5)) * 15/100) as money) else null end as Net15,	
+	p.Gross,
+	case when GstUpdated is null or GstUpdated = 'false' then 	
+	    cast( ((Gross * 100) / (100 + 12.5)) + (((Gross * 100) / (100 + 12.5)) * 15/100) as money) else null end as Gross15
 from PurchaseItem p
 inner join PurchaseLine l on p.PurchaseLineID = l.PurchaseLineID
 left join RequestStatus r on p.RequestStatusID = r.RequestStatusID
@@ -80,9 +125,17 @@ inner join [Country] c on s.CountryID = c.CountryID and c.CountryName like '%zea
 where {0}
 --where p.StartDate <= '2010.10.1 00:00:00' and EndDate >= '2010.10.1 00:00:00'
 --where StartDate >= '2010.10.1 00:00:00'
-and p.GstUpdated is null or p.GstUpdated = 'false'
+--and p.GstUpdated is null or p.GstUpdated = 'false'
 and Gross is not null and Gross > 0
 order by i.ItineraryName, l.PurchaseLineName, StartDate";
+
+        const string UpdateSql = 
+@"update [{0}] set
+	Net =   ((Net   * 100) / (100 + 12.5)) + (((Net   * 100) / (100 + 12.5)) * 15/100),
+	Gross = ((Gross * 100) / (100 + 12.5)) + (((Gross * 100) / (100 + 12.5)) * 15/100),
+	GstUpdated = 1 
+where {0}ID = {1};";
+
         // ---------------------------------------------------------------------------------------------------------------
         #endregion
 
@@ -102,10 +155,10 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
             comboBox1.Items.AddRange(new[]
                                          {
                                              "-- choose --",
-                                             "Supplier rates that span 1 Oct", 
-                                             "Supplier rates after 1 Oct",
-                                             "Itineary bookings that span 1 Oct", 
-                                             "Itineary bookings after 1 Oct"
+                                             "Supplier Rates that span 1 Oct", 
+                                             "Supplier Rates after 1 Oct",
+                                             "Itinerary Bookings that span 1 Oct", 
+                                             "Itinerary Bookings after 1 Oct"
                                          });
             comboBox1.SelectedIndex = 0;
         }
@@ -147,18 +200,95 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
             grid.DataSource = dv;
         }
 
-        private bool _isSelectAll = true;
+        private bool _isSelectAll;
         private void SelectAll()
         {
             _isSelectAll = !_isSelectAll;
             foreach (var row in grid.UltraGrid.Rows)
             {
                 if (row == null || row.GetType() == typeof(UltraGridEmptyRow) || row.GetType() == typeof(UltraGridGroupByRow))
-                    return;
+                    continue;
+
+                if (_isSelectAll && row.Cells["GstUpdated"].Value != DBNull.Value && (bool)row.Cells["GstUpdated"].Value)
+                    continue;
 
                 row.Cells["IsSelected"].Value = _isSelectAll;
             }
         }
+        
+        private void ProcessUpdate()
+        {
+            string sqlBatch;
+
+            // prepare --------------------
+            if (!App.AskYesNo("Step 1 of 3:\r\n\r\nReady to prepare data for selected rows.\r\n\r\nContinue?")) return;
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+
+                var list = grid.GetDataRowsList();
+                if (list.Count == 0) { App.ShowInfo("No rows selected, cancelling process..."); return; }
+
+                sqlBatch = CreateUpdateScript(list);
+                if (sqlBatch.Length == 0) { App.ShowInfo("Update script is empty (no rows selected?), cancelling process..."); return; }
+            } 
+            finally { Cursor = Cursors.Default; }
+            
+            // backup ----------------------
+            if (!App.AskYesNo("Step 2 of 3:\r\n\r\nReady to create database restore point.\r\n\r\nContinue?")) return;
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                BackupDb();
+            } 
+            finally { Cursor = Cursors.Default; }
+
+            // update -----------------------
+            if (!App.AskYesNo("Step 3 of 3:\r\n\r\nReady to commit selected rows to the database.\r\n\r\n*FINAL* Continue?")) return;
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                UpdateToDb(sqlBatch);
+                btnLoad.PerformClick();
+                App.ShowInfo("Update completed.\r\n\r\nNOTE: updated rows will now be disabled.");
+            }
+            catch (Exception ex) { App.Error("Oops, an error occurred! But its OK, all changes have been ROLLED BACK to previous state. Please work through this issue with TourWriter support staff.", ex, true); }
+            finally { Cursor = Cursors.Default; }
+        }
+
+        private static string CreateUpdateScript(IList<UltraGridRow> list)
+        {
+            var isItinerary = list[0].Cells.Exists("PurchaseItemID");
+
+            var batch = new StringBuilder();
+            foreach (var row in list)
+            {
+                if (row == null) continue;
+                if (row.Cells["IsSelected"].Value != DBNull.Value && !(bool)row.Cells["IsSelected"].Value) continue;
+                if (row.Cells["GstUpdated"].Value != DBNull.Value && (bool)row.Cells["GstUpdated"].Value) continue;
+
+                var name = isItinerary
+                               ? "PurchaseItem"
+                               : "Option";
+                var id = isItinerary
+                             ? (int)row.Cells["PurchaseItemID"].Value
+                             : (int)row.Cells["OptionID"].Value;
+
+                batch.AppendLine(string.Format(UpdateSql, name, id));
+            }
+            return batch.ToString();
+        }
+
+        private static void BackupDb()
+        {
+            DatabaseHelper.Backup("TourWriter-PreGst-" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".bak");
+        }
+
+        private static void UpdateToDb(string sqlBatch)
+        {
+            DatabaseHelper.RunScriptWithTransaction(sqlBatch);
+        }
+
 
         private static void grid_InitializeLayout(object sender, InitializeLayoutEventArgs e)
         {
@@ -167,7 +297,10 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
 
             foreach (var c in e.Layout.Bands[0].Columns)
             {
-                if (c.Key == "SupplierFolderID" || c.Key == "ItineraryFolderID") { c.Hidden = true; }
+                if (c.Key == "OptionID" || 
+                    c.Key == "PurchaseItemID" ||
+                    c.Key == "SupplierFolderID" ||
+                    c.Key == "SupplierFolderID") { c.Hidden = true; }
 
                 else if (c.Key == "IsSelected")
                 {
@@ -181,6 +314,10 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
                     c.Width = 30;
                     c.MaxWidth = 30;
                     c.SortIndicator = SortIndicator.Disabled;
+                }
+                else if (c.Key == "GstUpdated")
+                {
+                    c.Header.Caption = "Done";
                 }
                 else if (c.Key.Contains("Net") || c.Key.Contains("Gross"))
                 {
@@ -197,8 +334,10 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
 
         private void grid_InitializeRow(object sender, InitializeRowEventArgs e)
         {
-            if (e.Row.Cells["IsSelected"].Value == null)
-                e.Row.Cells["IsSelected"].Value = true;
+            if (e.Row.Cells["IsSelected"].Value == null) e.Row.Cells["IsSelected"].Value = true;
+
+            var isUpdated = e.Row.Cells["GstUpdated"].Value != DBNull.Value && (bool) e.Row.Cells["GstUpdated"].Value;
+            if (isUpdated) e.Row.Activation = Activation.Disabled;
         }
 
         private void gridBookings_MouseClick(object sender, MouseEventArgs e)
@@ -225,10 +364,11 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
-            btnSupplier.Enabled = btnItinerary.Enabled = true;
 
             LoadData(comboBox1.SelectedIndex);
-            if (comboBox1.SelectedIndex < 3) btnItinerary.Enabled = false;
+
+            btnItinerary.Enabled = comboBox1.SelectedIndex > 2;
+            btnGo.Enabled = btnSupplier.Enabled = btnLoad.Enabled = comboBox1.SelectedIndex > 0;
 
             Cursor = Cursors.Default;
         }
@@ -283,5 +423,29 @@ order by i.ItineraryName, l.PurchaseLineName, StartDate";
             return "Nz GST adjust";
         }
         #endregion
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            ProcessUpdate();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            var form = new Form {Size = new System.Drawing.Size(900, 900)};
+            var txt = new TextBox
+                          {
+                              Multiline = true,
+                              Dock = DockStyle.Fill,
+                              ReadOnly = true,
+                              Text = InfoMsg,
+                              SelectedText = "",
+                              Font = new System.Drawing.Font("Ariel", 12),
+                              ScrollBars = ScrollBars.Both
+                          };
+            txt.SelectionStart = 0;
+            txt.SelectionLength = 0;
+            form.Controls.Add(txt);
+            form.Show();
+        }
     }
 }
--- TourWriter/Modules/Plugins/NzGst/GstAdjust.designer.cs
+++ TourWriter/Modules/Plugins/NzGst/GstAdjust.designer.cs
@@ -30,13 +30,13 @@ namespace TourWriter.Modules.Plugins.NzGst
         {
             this.txtTo = new Infragistics.Win.UltraWinEditors.UltraDateTimeEditor();
             this.txtFrom = new Infragistics.Win.UltraWinEditors.UltraDateTimeEditor();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnLoad = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.grid = new TourWriter.UserControls.DataExtractGrid();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
             this.btnItinerary = new System.Windows.Forms.Button();
             this.btnSupplier = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -61,23 +61,9 @@ namespace TourWriter.Modules.Plugins.NzGst
             this.txtFrom.TabIndex = 67;
             this.txtFrom.Visible = false;
             // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(12, 478);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(807, 13);
-            this.label3.TabIndex = 69;
-            this.label3.Text = "Calculation used to adjust Net and Gross price: ((Amount*100) / (100+12.5)) + (((" +
-                "Amount*100) / (100+12.5)) * 15/100), eg remove old GST 12.5% then add new GST 15" +
-                "%.";
-            // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(290, 12);
+            this.btnLoad.Location = new System.Drawing.Point(298, 12);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(75, 24);
             this.btnLoad.TabIndex = 73;
@@ -89,7 +75,7 @@ namespace TourWriter.Modules.Plugins.NzGst
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(72, 15);
+            this.comboBox1.Location = new System.Drawing.Point(80, 15);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(212, 21);
             this.comboBox1.TabIndex = 75;
@@ -101,9 +87,9 @@ namespace TourWriter.Modules.Plugins.NzGst
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label1.Location = new System.Drawing.Point(13, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 76;
-            this.label1.Text = "View data";
+            this.label1.Text = "Choose data";
             // 
             // grid
             // 
@@ -113,14 +99,15 @@ namespace TourWriter.Modules.Plugins.NzGst
             this.grid.DataSource = null;
             this.grid.ExportFileName = "";
             this.grid.GridEnabled = true;
-            this.grid.Location = new System.Drawing.Point(16, 20);
+            this.grid.Location = new System.Drawing.Point(16, 42);
             this.grid.Name = "grid";
-            this.grid.Size = new System.Drawing.Size(805, 435);
+            this.grid.Size = new System.Drawing.Size(891, 454);
             this.grid.TabIndex = 74;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnHelp);
+            this.panel1.Controls.Add(this.btnGo);
             this.panel1.Controls.Add(this.btnItinerary);
             this.panel1.Controls.Add(this.btnSupplier);
             this.panel1.Controls.Add(this.label4);
@@ -129,30 +116,40 @@ namespace TourWriter.Modules.Plugins.NzGst
             this.panel1.Controls.Add(this.btnLoad);
             this.panel1.Controls.Add(this.txtFrom);
             this.panel1.Controls.Add(this.txtTo);
-            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.grid);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 40);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(836, 499);
+            this.panel1.Size = new System.Drawing.Size(922, 499);
             this.panel1.TabIndex = 77;
             // 
-            // label2
+            // btnHelp
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(13, 462);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(578, 13);
-            this.label2.TabIndex = 81;
-            this.label2.Text = "Filters used to select data: Dates as selected above, Supplier country contains \'" +
-                "zealand\', Gross price is a positive number.";
+            this.btnHelp.ForeColor = System.Drawing.Color.Blue;
+            this.btnHelp.Image = global::TourWriter.Properties.Resources.Information;
+            this.btnHelp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHelp.Location = new System.Drawing.Point(379, 12);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 24);
+            this.btnHelp.TabIndex = 83;
+            this.btnHelp.Text = "Info";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(470, 12);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(121, 24);
+            this.btnGo.TabIndex = 82;
+            this.btnGo.Text = "Begin Update Steps";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
             // 
             // btnItinerary
             // 
-            this.btnItinerary.Location = new System.Drawing.Point(572, 12);
+            this.btnItinerary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnItinerary.Location = new System.Drawing.Point(830, 12);
             this.btnItinerary.Name = "btnItinerary";
             this.btnItinerary.Size = new System.Drawing.Size(75, 24);
             this.btnItinerary.TabIndex = 80;
@@ -162,7 +159,8 @@ namespace TourWriter.Modules.Plugins.NzGst
             // 
             // btnSupplier
             // 
-            this.btnSupplier.Location = new System.Drawing.Point(491, 12);
+            this.btnSupplier.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSupplier.Location = new System.Drawing.Point(749, 12);
             this.btnSupplier.Name = "btnSupplier";
             this.btnSupplier.Size = new System.Drawing.Size(75, 24);
             this.btnSupplier.TabIndex = 79;
@@ -172,9 +170,10 @@ namespace TourWriter.Modules.Plugins.NzGst
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label4.Location = new System.Drawing.Point(413, 18);
+            this.label4.Location = new System.Drawing.Point(671, 18);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 78;
@@ -185,7 +184,7 @@ namespace TourWriter.Modules.Plugins.NzGst
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(836, 539);
+            this.ClientSize = new System.Drawing.Size(922, 539);
             this.Controls.Add(this.panel1);
             this.HeaderVisible = true;
             this.Name = "GstAdjust";
@@ -203,7 +202,6 @@ namespace TourWriter.Modules.Plugins.NzGst
 
         private Infragistics.Win.UltraWinEditors.UltraDateTimeEditor txtTo;
         private Infragistics.Win.UltraWinEditors.UltraDateTimeEditor txtFrom;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnLoad;
         private TourWriter.UserControls.DataExtractGrid grid;
         private System.Windows.Forms.ComboBox comboBox1;
@@ -212,6 +210,7 @@ namespace TourWriter.Modules.Plugins.NzGst
         private System.Windows.Forms.Button btnItinerary;
         private System.Windows.Forms.Button btnSupplier;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnHelp;
     }
 }
