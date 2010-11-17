33a8c9fc44c1842ad55247f3c53eedfb3a19c466
Seba Illingworth <seba.illingworth@gmail.com>
2010-11-17 21:14:28 +1300

    Use new currency exch service

--- TourWriter/Services/CurrencyService.cs
+++ TourWriter/Services/CurrencyService.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading;
-using System.Web.Services;
-using System.Web.Services.Description;
-using System.Web.Services.Protocols;
 using TourWriter.Global;
 using TourWriter.Info;
 
@@ -14,9 +10,6 @@ namespace TourWriter.Services
 {
     class CurrencyService
     {
-        private const string updateUri =
-            @"http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency={0}&ToCurrency={1}";
-
         internal class Currency
         {
             public object Key { get; set; }
@@ -41,30 +34,19 @@ namespace TourWriter.Services
             return new RegionInfo(CultureInfo.CurrentCulture.LCID).ISOCurrencySymbol;
         }
 
-        /// <summary>
-        /// Get the currency exchange rates for a list of currencies, from a currency webservice.
-        /// Usage:
-        ///    var currencies = new List<CurrencyUpdateService.Currency>();
-        ///    currencies.Add(new CurrencyUpdateService.Currency { FromCurrency = "NZD", ToCurrency = "AUD" });
-        ///    CurrencyUpdateService.GetRates(currencies);
-        ///    foreach(var c in currencies)
-        ///        Console.WriteLine(c.Rate);
-        /// </summary>
-        /// <param name="currencies">List of currency objects with from and to currency codes.</param>
-        /// <returns>List of currency objects with updated exchange rates.</returns>
         internal static List<Currency> GetRates(List<Currency> currencies)
         {
             // Start threads
             var threads = new List<Thread>();
-            for (int i = 0; i < currencies.Count; i++)
+            for (var i = 0; i < currencies.Count; i++) // GetRateHttp(currencies[i]);
             {
                 var currency = currencies[i];
-                var thread = new Thread(() => GetRateHttp(currency)) {Name = string.Format("Currency_GetRateHttp{0}", i)};
+                var thread = new Thread(() => GetRateHttp(currency)) { Name = string.Format("Currency_GetRateHttp{0}", i) };
                 thread.Start();
                 threads.Add(thread);
             }
             // Wait for threads to finish
-            foreach (Thread thread in threads)
+            foreach (var thread in threads)
             {
                 thread.Join();
             }
@@ -72,12 +54,6 @@ namespace TourWriter.Services
             return currencies;
         }
 
-        /// <summary>
-        /// Get a currency exchange rate.
-        /// </summary>
-        /// <param name="fromCurrency">The currency to convert from.</param>
-        /// <param name="toCurrency">The currency to convert to.</param>
-        /// <returns>The currency rate.</returns>
         internal static double? GetRate(string fromCurrency, string toCurrency)
         {
             if (fromCurrency == toCurrency || string.IsNullOrEmpty(fromCurrency.Trim()) || string.IsNullOrEmpty(toCurrency.Trim()))
@@ -104,62 +80,6 @@ namespace TourWriter.Services
         {
             if (fromCurrency.ToLower().Trim() == toCurrency.ToLower().Trim()) return 1;
 
-            if (App.IsDebugMode ||
-                Cache.ToolSet.AppSettings.Rows.Count > 0 && Cache.ToolSet.AppSettings[0].InstallID.ToString().ToLower() == "13b8e136-405f-402f-a4bb-3913879be702".ToLower() || // dev
-                Cache.ToolSet.AppSettings.Rows.Count > 0 && Cache.ToolSet.AppSettings[0].InstallID.ToString().ToLower() == "575E7900-BF13-42D1-A661-2242510C3359".ToLower() || // te
-                Cache.ToolSet.AppSettings.Rows.Count > 0 && Cache.ToolSet.AppSettings[0].InstallID.ToString().ToLower() == "aa08e99f-8e02-4b7d-882a-e36eadd213de".ToLower()) // harald
-                return GetRateHttpTest(fromCurrency, toCurrency);
-
-            string request = string.Format(updateUri, fromCurrency, toCurrency);
-            string response = new WebClient().DownloadString(request);
-
-            var reg = Regex.Match(response, @"[0-9]+\.[0-9]+(?=<\/double>)");
-
-            double result;
-            if (double.TryParse(reg.Value, NumberStyles.Any, CultureInfo.CreateSpecificCulture("en-US"), out result))
-                return result;
-
-            return 0;
-        }
-
-        private static Currency GetRateSoap(Currency currency)
-        {
-            try
-            {
-                currency.Rate = GetRateSoap(currency.FromCurrency, currency.ToCurrency);
-            }
-            catch (Exception ex)
-            {
-                // load error to the message field
-                currency.ErrorMessage = ex.ToString();
-            }
-            return currency;
-        }
-
-        private static double GetRateSoap(string fromCurrency, string toCurrency)
-        {
-            var client = new CurrencyConvertorClient();
-            return client.ConversionRate(fromCurrency, toCurrency);
-        }
-
-        [WebServiceBinding(Name = "CurrencyConvertorSoap", Namespace = "http://www.webserviceX.NET/")]
-        private class CurrencyConvertorClient : SoapHttpClientProtocol
-        {
-            public CurrencyConvertorClient()
-            {
-                Url = "http://www.webservicex.net/CurrencyConvertor.asmx";
-            }
-
-            [SoapDocumentMethodAttribute("http://www.webserviceX.NET/ConversionRate", RequestNamespace = "http://www.webserviceX.NET/", ResponseNamespace = "http://www.webserviceX.NET/", Use = SoapBindingUse.Literal, ParameterStyle = SoapParameterStyle.Wrapped)]
-            public double ConversionRate(string FromCurrency, string ToCurrency)
-            {
-                object[] results = Invoke("ConversionRate", new object[] { FromCurrency, ToCurrency });
-                return ((double)(results[0]));
-            }
-        }
-
-        private static double GetRateHttpTest(string fromCurrency, string toCurrency)
-        {
             const string url = "http://finance.yahoo.com/d/quotes.csv?s={0}{1}=X&f=l1&e=.csv";
 
             double result;
