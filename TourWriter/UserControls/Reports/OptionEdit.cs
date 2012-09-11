df0adc3e6cd1b672789cbf402d5d516dfedb55df
Seba Illingworth <seba.illingworth@gmail.com>
2012-09-11 20:59:17 +1200

    Add city to report options

--- TourWriter/UserControls/Reports/OptionEdit.cs
+++ TourWriter/UserControls/Reports/OptionEdit.cs
@@ -107,6 +107,13 @@ namespace TourWriter.UserControls.Reports
                     (editor as CheckBoxSet).Initialise(Cache.ToolSet.Branch.Where(x => x.RowState != DataRowState.Deleted).OrderBy(x => x.BranchName).CopyToDataTable(), "BranchID", "BranchName", true);
                     (editor as CheckBoxSet).CheckAll(true);
                     break;
+                case "@CityIDList":
+                    label.Text = "City";
+                    editor = new CheckBoxSet();
+                    if (Cache.ToolSet.City.Count == 0) break;
+                    (editor as CheckBoxSet).Initialise(Cache.ToolSet.City.Where(x => x.RowState != DataRowState.Deleted).OrderBy(x => x.CityName).CopyToDataTable(), "CityID", "CityName", true);
+                    (editor as CheckBoxSet).CheckAll(true);
+                    break;
                 case "@DepartmentIDList":
                     label.Text = "Department";
                     editor = new CheckBoxSet();
