ff685e27c055040345795209367c23e8f5f17b3b
Seba Illingworth <seba.illingworth@gmail.com>
2012-10-05 15:54:45 +1300

    Add SortDate col to db and ds

---
+++ TourWriter/Services/Update/DatabaseUpdateScripts/2012.10.05.sql
@@ -0,0 +1,864 @@
+﻿/* TourWriter database update script */
+GO
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING ON
+GO
+SET ANSI_WARNINGS ON
+GO
+SET CONCAT_NULL_YIELDS_NULL ON
+GO
+SET ARITHABORT ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+GO
+BEGIN TRANSACTION
+GO
+if ((select VersionNumber from AppSettings) <> '2012.08.16')
+	RAISERROR (N'Database Update Script is not correct version for current database version',17,1)
+
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+----------------------------------------------------------------------------------------
+
+GO
+PRINT N'Add new column SortDate...';
+
+GO
+
+if not Exists(select * from sys.columns where Name = N'SortDate' and Object_ID = Object_ID(N'PurchaseItem'))
+	ALTER TABLE [dbo].[PurchaseItem]
+		ADD [SortDate] DATETIME NULL;
+GO
+
+
+GO
+PRINT N'Altering [dbo].[_ItinerarySet_Sel_ByID]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[_ItinerarySet_Sel_ByID]
+@ItineraryID INT
+AS
+SET NOCOUNT ON
+
+-- PaymentTerm --
+SELECT
+	[PaymentTermID],
+	[PaymentDueID],
+	[PaymentDuePeriod],
+	[DepositAmount],
+	[DepositType],
+	[DepositDueID],
+	[DepositDuePeriod]
+FROM [dbo].[PaymentTerm]
+WHERE --*** SalePaymentTerms - do we need client or/else itinerary or/else agent? ***
+	[PaymentTermID] = -- SalePaymentTerms from Itinerary
+		(SELECT [PaymentTermID] FROM [Itinerary] WHERE [ItineraryID] = @ItineraryID)
+OR
+	[PaymentTermID] IN -- PurchasePaymentTerms from PurchaseItem
+		(SELECT DISTINCT [PaymentTermID] FROM [PurchaseItem] WHERE
+			[PurchaseLineID] IN ( 
+				SELECT [PurchaseLineID] FROM [dbo].[PurchaseLine] 
+					WHERE [ItineraryID] = @ItineraryID ))
+
+
+-- *************************** Itinerary ****************************
+
+-- Itinerary --
+SELECT
+	[ItineraryID],
+	[ItineraryName],
+	[DisplayName],
+	[CustomCode],
+	[ArriveDate],
+	[ArriveCityID],
+	[ArriveFlight],
+	[ArriveNote],
+	[DepartDate],
+	[DepartCityID],
+	[DepartFlight],
+	[DepartNote],
+	[NetComOrMup],
+	[NetMargin],
+	[GrossMarkup],
+	[GrossOverride],
+	[IsLockedGrossOverride],
+	[PricingNote],
+	[AgentID],
+	[PaymentTermID],
+	[ItineraryStatusID],
+	[ItinerarySourceID],
+	[CountryID],
+	[AssignedTo],
+	[DepartmentID],
+	[BranchID],
+	[PaxOverride],
+	[Comments],
+	[IsRecordActive],
+	[IsReadOnly],
+	[ParentFolderID],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[IsDeleted],
+	[CurrencyCode],
+	[NetMinOrMax],
+	[AgentContactID]
+FROM [dbo].[Itinerary]
+WHERE
+	[ItineraryID] = @ItineraryID
+
+-- ItineraryPax --
+SELECT
+	[ItineraryPaxID],
+	[ItineraryPaxName],
+	[ItineraryID],
+	[MemberCount],
+	[MemberRooms],
+	[StaffCount],
+	[StaffRooms],
+	[GrossMarkup],
+	[GrossOverride]
+FROM [dbo].[ItineraryPax]
+WHERE
+	[ItineraryID] = @ItineraryID
+	
+-- ItineraryMarginOverride --	
+SELECT
+	[ItineraryID],
+	[ServiceTypeID],
+	[Margin]
+FROM [dbo].[ItineraryMarginOverride]
+WHERE
+	[ItineraryID] = @ItineraryID
+	
+-- PurchaseLine --
+SELECT
+	[PurchaseLineID],
+	[ItineraryID],
+	[SupplierID],
+	[PurchaseLineName],
+	[Comments],
+	[NoteToSupplier],
+	[NoteToVoucher],
+	[NoteToClient],
+	[Approved],
+	[SupplierReference],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion]
+FROM [dbo].[PurchaseLine]
+WHERE
+	[ItineraryID] = @ItineraryID
+
+-- PurchaseItem --
+SELECT
+	i.[PurchaseItemID],
+	i.[PurchaseLineID],
+	i.[OptionID],
+	i.[PurchaseItemName],
+	i.[BookingReference],
+	i.[StartDate],
+	i.[StartTime],
+	i.[EndDate],
+	i.[EndTime],
+	i.[Net],
+	i.[Gross],
+	i.[CurrencyRate],
+	i.[Quantity],
+	i.[NumberOfDays],
+	i.[PaymentTermID],
+	i.[RequestStatusID],
+	i.[IsLockedAccounting],
+	i.[AddedOn],
+	i.[AddedBy],
+	i.[RowVersion],
+	i.[DiscountUnits],
+	i.[DiscountType],
+	i.[IsInvoiced],
+	i.[SortDate],
+	-- add lookup columns
+	r.[RateID],
+	s.[ServiceID],
+	o.[OptionTypeID],
+	s.[ServiceTypeID],
+	o.[OptionName],
+	s.[ServiceName],
+	stype.[ServiceTypeName],
+	otype.[OptionTypeName],	
+	s.[ChargeType],
+	o.[IsDefault] as IsDefaultOptionType,
+	s.[CurrencyCode]
+FROM [dbo].[PurchaseItem] i 
+INNER JOIN [Option] o ON o.OptionID = i.OptionID
+INNER JOIN [Rate] r ON r.RateID = o.RateID
+INNER JOIN [Service] s ON s.ServiceID = r.ServiceID
+left outer join ServiceType as stype on s.ServiceTypeID = stype.ServiceTypeID
+left outer join OptionType as otype on o.OptionTypeID = otype.OptionTypeID
+WHERE [PurchaseLineID] IN ( 
+		SELECT [PurchaseLineID] FROM [dbo].[PurchaseLine] 
+		WHERE [ItineraryID] = @ItineraryID )	
+ORDER BY [PurchaseLineID], [StartDate]
+
+-- PurchaseItemNote --
+
+SELECT
+	[PurchaseItemNoteID],
+	[PurchaseItemID],
+	[FlagID],
+	[Note]
+FROM [dbo].[PurchaseItemNote]
+WHERE [PurchaseItemID] IN (
+	SELECT [PurchaseItemID] 
+	FROM [dbo].[PurchaseItem] item 
+	join PurchaseLine line on line.PurchaseLineID = item.PurchaseLineID
+	WHERE [ItineraryID] = @ItineraryID )	
+ORDER BY [FlagID], [PurchaseItemNoteID]
+
+-- *********************** Members ********************************************
+
+-- Contact --		
+SELECT
+	[ContactID],
+	[ContactName],
+	[Title],
+	[FirstName],
+	[LastName],
+	[StreetAddress],
+	[PostName],
+	[PostAddress],
+	[CityID],
+	[RegionID],
+	[StateID],
+	[CountryID],
+	[PostCode],
+	[WorkPhone],
+	[HomePhone],
+	[CellPhone],
+	[Fax],
+	[Email1],
+	[Email2],
+	[Website],
+	[BirthDate],
+	[Notes],
+	[ParentFolderID],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[IsDeleted]
+FROM [dbo].[Contact]
+WHERE
+	[ContactID] IN (
+		SELECT c.[ContactID] 
+		FROM   [dbo].[ItineraryMember] c, [dbo].[ItineraryGroup] b 
+		WHERE  b.ItineraryID = @ItineraryID 
+		AND    c.[ItineraryGroupID] = b.[ItineraryGroupID] )
+			
+-- ItineraryGroup --
+SELECT
+	[ItineraryGroupID],
+	[ItineraryID],
+	[ItineraryGroupName],
+	[Comments],
+	[NoteToClient],
+	[NoteToSupplier],
+	[CurrencyCode],
+	[CurrencyRate],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion]
+FROM [dbo].[ItineraryGroup]
+WHERE
+	[ItineraryID] = @ItineraryID
+
+-- ItineraryMember --
+SELECT
+	[ItineraryMemberID],
+	[ItineraryGroupID],
+	[ItineraryMemberName],
+	[ContactID],
+	[AgeGroupID],
+	[Age],
+	[ArriveDate],
+	[ArriveCityID],
+	[ArriveFlight],
+	[DepartDate],
+	[DepartCityID],
+	[DepartFlight],
+	[IsDefaultContact],
+	[IsDefaultBilling],
+	[Comments],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[Title]
+FROM [dbo].[ItineraryMember]
+WHERE
+	[ItineraryGroupID] IN ( 
+		SELECT [ItineraryGroupID] FROM [dbo].[ItineraryGroup] 
+		WHERE [ItineraryID] = @ItineraryID )
+
+-- *********************** Messages *******************************************
+
+-- Message --
+SELECT
+	[MessageID],
+	[MessageName],
+	[MessageType],
+	[MessageTo],
+	[MessageFrom],
+	[MessageFile],
+	[MessageDate],
+	[Description],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion]
+FROM [dbo].[Message]
+WHERE
+	[MessageID] IN (
+		SELECT [MessageID] FROM [dbo].[ItineraryMessage]
+		WHERE [ItineraryID] = @ItineraryID)
+
+-- ItineraryMessage --
+SELECT
+	[ItineraryID],
+	[MessageID],
+	[AddedOn],
+	[AddedBy]
+FROM [dbo].[ItineraryMessage]
+	WHERE [ItineraryID] = @ItineraryID
+
+-- SupplierMessage (we don't want select data, just a placeholder for inserts) --
+SELECT
+	[SupplierID],
+	[MessageID],
+	[AddedOn],
+	[AddedBy]
+FROM [dbo].[SupplierMessage]
+WHERE [SupplierID] = NULL AND[MessageID] = null
+
+-- *********************** Lookup Tables **********************
+
+-- Supplier Lookup --
+SELECT DISTINCT  
+	[SupplierID],
+	[SupplierName],
+	[HostName],
+	[Phone],
+	[MobilePhone],
+	[FreePhone],
+	[Fax],
+	[Email],
+	[GradeID],
+	[GradeExternalID],
+	[StreetAddress],
+	[PostAddress],
+	[CityID],
+	[RegionID],
+	[StateID],
+	[CountryID],
+	[NetTaxTypeID],
+	[GrossTaxTypeID]
+FROM [dbo].[Supplier] 
+WHERE [SupplierID] IN (
+	SELECT [SupplierID] 
+	FROM [dbo].[PurchaseLine] 
+	WHERE[ItineraryID] = @ItineraryID )
+
+-- Option Lookup --
+SELECT DISTINCT  
+	op.[OptionID],
+	op.[OptionName],
+	sv.[ServiceName],
+	sp.[SupplierID],
+	sv.[ServiceTypeID],
+	op.[OptionTypeID],	
+	sv.[CheckinTime],	
+	sv.[CheckoutTime],
+	sv.[CheckinMinutesEarly],
+	sv.[CurrencyCode],
+	op.[Net],
+	op.[Gross],
+	op.[PricingOption],
+	rt.[ValidFrom],
+	rt.[ValidTo]
+FROM [dbo].[Option] op 
+INNER JOIN [dbo].[Rate] rt     ON op.[RateID]     = rt.[RateID] 
+INNER JOIN [dbo].[Service] sv  ON rt.[ServiceID]   = sv.[ServiceID]
+INNER JOIN [dbo].[Supplier] sp ON sv.[SupplierID] = sp.[SupplierID]
+WHERE op.[OptionID] IN (
+	SELECT [OptionID] 
+	FROM [dbo].[PurchaseItem] i, [dbo].[PurchaseLine] l 
+	WHERE i.[PurchaseLineID] = l.[PurchaseLineID]
+	AND [ItineraryID] = @ItineraryID )
+
+-- SupplierText --
+SELECT
+	[SupplierTextID],
+	[SupplierTextName],
+	[SupplierID],
+	[FileName],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion]
+FROM [dbo].[SupplierText]
+WHERE [SupplierID] IN (
+	SELECT [SupplierID] 
+	FROM [dbo].[PurchaseLine] 
+	WHERE[ItineraryID] = @ItineraryID )
+
+-- *********************** Publising *******************************************
+SELECT
+	[ItineraryPubFileID],
+	[ItineraryPubFileName],
+	[ItineraryID],
+	[DayTemplateFile],
+	[Layout],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion]
+FROM [dbo].[ItineraryPubFile]
+WHERE
+	[ItineraryID] = @ItineraryID
+
+
+-- *********************** Payments *******************************************
+SELECT
+	[ItineraryPaymentID],
+	[ItineraryMemberID],
+	[Comments],
+	[Amount],
+	[PaymentDate],
+	[PaymentTypeID],
+	[ItinerarySaleID],
+	[IsLockedAccounting],
+	[RowVersion]
+FROM [dbo].ItineraryPayment
+WHERE
+	[ItineraryMemberID] IN ( 
+		SELECT m.[ItineraryMemberID] 
+		FROM [dbo].[ItineraryMember] m, [dbo].[ItineraryGroup] g 
+		WHERE g.[ItineraryID] = @ItineraryID
+		AND m.[ItineraryGroupID] = g.[ItineraryGroupID] )
+ORDER BY 
+	[ItineraryPaymentID] ASC
+
+-- *********************** Sales **********************************************
+SELECT
+	[ItinerarySaleID],
+	[ItineraryID],
+	[Comments],
+	[SaleDate],
+	[IsLockedAccounting]
+FROM [dbo].[ItinerarySale]
+WHERE [ItineraryID] = @ItineraryID
+
+SELECT
+	alloc.[ItinerarySaleID],
+	alloc.[ServiceTypeID],
+	[Amount]
+FROM [dbo].[ItinerarySaleAllocation] alloc
+LEFT OUTER JOIN [dbo].[ItinerarySale] sale ON alloc.ItinerarySaleID = sale.ItinerarySaleID
+WHERE sale.[ItineraryID] = @ItineraryID
+
+-- Discounts --
+
+SELECT
+	[DiscountID],
+	[ServiceID],
+	[UnitsUsed],
+	[UnitsFree],
+	[DiscountType]
+FROM [dbo].[Discount]
+WHERE
+	[ServiceID] in 
+	(	
+		select distinct s.ServiceID
+		from purchaseitem i
+		inner join PurchaseLine line on i.PurchaseLineID = line.PurchaseLineID 
+			and line.ItineraryID = @ItineraryID
+		left outer join [Option] o on o.OptionID = i.OptionID
+		left outer join [Rate] r on r.RateID = o.RateID
+		left outer join [Service] s on s.ServiceID = r.ServiceID 
+	)
+	
+-- GROUPS --		
+SELECT
+	o.[PurchaseItemID],
+	o.[ItineraryPaxID],
+	o.[MemberCount],
+	o.[MemberRooms],
+	o.[StaffCount],
+	o.[StaffRooms]
+FROM [dbo].[ItineraryPaxOverride] o
+inner join PurchaseItem i on o.PurchaseItemID = i.PurchaseItemID
+inner join PurchaseLine l on i.PurchaseLineID = l.PurchaseLineID
+WHERE l.ItineraryID = @ItineraryID
+
+-- ServiceWarnings
+SELECT [ServiceWarningID]
+		,[ServiceID]
+		,[Description]
+		,[ValidFrom]
+		,[ValidTo]
+	FROM [ServiceWarning]
+WHERE
+	[ServiceID] in 
+	(	
+		select distinct s.ServiceID
+		from purchaseitem i
+		inner join PurchaseLine line on i.PurchaseLineID = line.PurchaseLineID 
+			and line.ItineraryID = @ItineraryID
+		left outer join [Option] o on o.OptionID = i.OptionID
+		left outer join [Rate] r on r.RateID = o.RateID
+		left outer join [Service] s on s.ServiceID = r.ServiceID 
+	)
+AND ValidTo > getdate()
+
+-- TASKS --
+SELECT
+	t.[TaskID],
+	[TaskName],
+	[DateDue],
+	[DateCompleted],
+	[Note]
+FROM [dbo].[Task] t
+inner join ItineraryTask i on i.TaskID = t.TaskID
+where i.ItineraryID = @ItineraryID
+
+-- ItineraryTask --
+SELECT
+	[ItineraryTaskID],
+	[ItineraryID],
+	[TaskID]
+FROM [dbo].[ItineraryTask]
+where ItineraryID = @ItineraryID
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Ins]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Ins]
+	@PurchaseLineID int,
+	@OptionID int,
+	@PurchaseItemName varchar(255),
+	@BookingReference varchar(50),
+	@StartDate datetime,
+	@StartTime datetime,
+	@EndDate datetime,
+	@EndTime datetime,
+	@Net money,
+	@Gross money,
+	@CurrencyRate money,
+	@Quantity decimal(12, 4),
+	@NumberOfDays decimal(12, 4),
+	@PaymentTermID int,
+	@RequestStatusID int,
+	@IsLockedAccounting bit,
+	@AddedOn datetime,
+	@AddedBy int,
+	@DiscountUnits decimal(12, 4),
+	@DiscountType varchar(10),
+	@IsInvoiced bit,
+	@SortDate datetime,
+	@PurchaseItemID int OUTPUT
+AS
+INSERT [dbo].[PurchaseItem]
+(
+	[PurchaseLineID],
+	[OptionID],
+	[PurchaseItemName],
+	[BookingReference],
+	[StartDate],
+	[StartTime],
+	[EndDate],
+	[EndTime],
+	[Net],
+	[Gross],
+	[CurrencyRate],
+	[Quantity],
+	[NumberOfDays],
+	[PaymentTermID],
+	[RequestStatusID],
+	[IsLockedAccounting],
+	[AddedOn],
+	[AddedBy],
+	[DiscountUnits],
+	[DiscountType],
+	[IsInvoiced],
+	[SortDate]
+)
+VALUES
+(
+	@PurchaseLineID,
+	@OptionID,
+	@PurchaseItemName,
+	@BookingReference,
+	@StartDate,
+	@StartTime,
+	@EndDate,
+	@EndTime,
+	@Net,
+	@Gross,
+	@CurrencyRate,
+	@Quantity,
+	@NumberOfDays,
+	@PaymentTermID,
+	@RequestStatusID,
+	@IsLockedAccounting,
+	@AddedOn,
+	@AddedBy,
+	@DiscountUnits,
+	@DiscountType,
+	@IsInvoiced,
+	@SortDate
+)
+SELECT @PurchaseItemID=SCOPE_IDENTITY()
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Sel_All]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Sel_All]
+AS
+SET NOCOUNT ON
+SELECT
+	[PurchaseItemID],
+	[PurchaseLineID],
+	[OptionID],
+	[PurchaseItemName],
+	[BookingReference],
+	[StartDate],
+	[StartTime],
+	[EndDate],
+	[EndTime],
+	[Net],
+	[Gross],
+	[CurrencyRate],
+	[Quantity],
+	[NumberOfDays],
+	[PaymentTermID],
+	[RequestStatusID],
+	[IsLockedAccounting],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[DiscountUnits],
+	[DiscountType],
+	[IsInvoiced],
+	[SortDate]
+FROM [dbo].[PurchaseItem]
+ORDER BY 
+	[PurchaseItemID] ASC
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Sel_ByID]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Sel_ByID]
+	@PurchaseItemID int
+AS
+SET NOCOUNT ON
+SELECT
+	[PurchaseItemID],
+	[PurchaseLineID],
+	[OptionID],
+	[PurchaseItemName],
+	[BookingReference],
+	[StartDate],
+	[StartTime],
+	[EndDate],
+	[EndTime],
+	[Net],
+	[Gross],
+	[CurrencyRate],
+	[Quantity],
+	[NumberOfDays],
+	[PaymentTermID],
+	[RequestStatusID],
+	[IsLockedAccounting],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[DiscountUnits],
+	[DiscountType],
+	[IsInvoiced],
+	[SortDate]
+FROM [dbo].[PurchaseItem]
+WHERE
+	[PurchaseItemID] = @PurchaseItemID
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Sel_ByOptionID]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Sel_ByOptionID]
+	@OptionID int
+AS
+SET NOCOUNT ON
+SELECT
+	[PurchaseItemID],
+	[PurchaseLineID],
+	[OptionID],
+	[PurchaseItemName],
+	[BookingReference],
+	[StartDate],
+	[StartTime],
+	[EndDate],
+	[EndTime],
+	[Net],
+	[Gross],
+	[CurrencyRate],
+	[Quantity],
+	[NumberOfDays],
+	[PaymentTermID],
+	[RequestStatusID],
+	[IsLockedAccounting],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[DiscountUnits],
+	[DiscountType],
+	[IsInvoiced],
+	[SortDate]
+FROM [dbo].[PurchaseItem]
+WHERE
+	[OptionID] = @OptionID
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Sel_ByPurchaseLineID]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Sel_ByPurchaseLineID]
+	@PurchaseLineID int
+AS
+SET NOCOUNT ON
+SELECT
+	[PurchaseItemID],
+	[PurchaseLineID],
+	[OptionID],
+	[PurchaseItemName],
+	[BookingReference],
+	[StartDate],
+	[StartTime],
+	[EndDate],
+	[EndTime],
+	[Net],
+	[Gross],
+	[CurrencyRate],
+	[Quantity],
+	[NumberOfDays],
+	[PaymentTermID],
+	[RequestStatusID],
+	[IsLockedAccounting],
+	[AddedOn],
+	[AddedBy],
+	[RowVersion],
+	[DiscountUnits],
+	[DiscountType],
+	[IsInvoiced],
+	[SortDate]
+FROM [dbo].[PurchaseItem]
+WHERE
+	[PurchaseLineID] = @PurchaseLineID
+GO
+PRINT N'Altering [dbo].[PurchaseItem_Upd]...';
+
+
+GO
+
+ALTER PROCEDURE [dbo].[PurchaseItem_Upd]
+	@PurchaseItemID int,
+	@PurchaseLineID int,
+	@OptionID int,
+	@PurchaseItemName varchar(255),
+	@BookingReference varchar(50),
+	@StartDate datetime,
+	@StartTime datetime,
+	@EndDate datetime,
+	@EndTime datetime,
+	@Net money,
+	@Gross money,
+	@CurrencyRate money,
+	@Quantity decimal(12, 4),
+	@NumberOfDays decimal(12, 4),
+	@PaymentTermID int,
+	@RequestStatusID int,
+	@IsLockedAccounting bit,
+	@AddedOn datetime,
+	@AddedBy int,
+	@RowVersion timestamp,
+	@DiscountUnits decimal(12, 4),
+	@DiscountType varchar(10),
+	@IsInvoiced bit,
+	@SortDate datetime
+AS
+UPDATE [dbo].[PurchaseItem]
+SET 
+	[PurchaseLineID] = @PurchaseLineID,
+	[OptionID] = @OptionID,
+	[PurchaseItemName] = @PurchaseItemName,
+	[BookingReference] = @BookingReference,
+	[StartDate] = @StartDate,
+	[StartTime] = @StartTime,
+	[EndDate] = @EndDate,
+	[EndTime] = @EndTime,
+	[Net] = @Net,
+	[Gross] = @Gross,
+	[CurrencyRate] = @CurrencyRate,
+	[Quantity] = @Quantity,
+	[NumberOfDays] = @NumberOfDays,
+	[PaymentTermID] = @PaymentTermID,
+	[RequestStatusID] = @RequestStatusID,
+	[IsLockedAccounting] = @IsLockedAccounting,
+	[AddedOn] = @AddedOn,
+	[AddedBy] = @AddedBy,
+	[DiscountUnits] = @DiscountUnits,
+	[DiscountType] = @DiscountType,
+	[IsInvoiced] = @IsInvoiced,
+	[SortDate] = @SortDate
+WHERE
+	[PurchaseItemID] = @PurchaseItemID
+	AND [RowVersion] = @RowVersion
+GO
+
+
+
+
+exec __RefreshViews;
+GO
+----------------------------------------------------------------------------------------
+PRINT N'Updating [dbo].[AppSettings] version number'
+GO
+UPDATE [dbo].[AppSettings] SET [VersionNumber]='2012.10.05'
+GO
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT N'The transacted portion of the database update succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT N'The transacted portion of the database update failed.'
+GO
+DROP TABLE #tmpErrors
+GO
